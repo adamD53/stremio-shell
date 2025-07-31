41746dd8fe578951adf3498c85be23b361224fb3
adamD53 <fofororor@gmail.com>
2025-07-31 15:48:27 +0200

    Force higher version of OpenGL, change the way of loading qml

--- main.cpp
+++ main.cpp
@@ -78,7 +78,14 @@ int main(int argc, char **argv)
     Application::setOrganizationName("Smart Code ltd");
     Application::setOrganizationDomain("stremio.com");
 
+    QSurfaceFormat format;
+    format.setRenderableType(QSurfaceFormat::OpenGL);
+    format.setVersion(3, 2);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(format);
+
     QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
+    qputenv("QT_QUICK_BACKEND", "opengl");
 
     MainApp app(argc, argv, true);
     #ifndef Q_OS_MACOS
@@ -110,7 +117,7 @@ int main(int argc, char **argv)
 
     InitializeParameters(engine, app); 
 
-    engine->load(QUrl(QStringLiteral("qrc:/main.qml")));
+    engine->loadFromModule("stremio",  "Main");
 
     #ifndef Q_OS_MACOS
     QObject::connect( &app, &SingleApplication::receivedMessage, &app, &MainApp::processMessage );
